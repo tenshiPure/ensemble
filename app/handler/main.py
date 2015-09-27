6b1748e47d8f3411a28ef08faca2af8bd9cf0ffd
tenshiPure <user.ryo@gmail.com>
2015-09-27 23:32:16 +0900

    login require

--- app/handler/main.py
+++ app/handler/main.py
@@ -3,4 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
+		if self.get_secure_cookie('sid') is None:
+			self.redirect('/login')
+
 		self.render('main.html')
