aa1f6c4cc02d029f37af42f9d75c939ce74a09e7
tenshiPure <user.ryo@gmail.com>
2015-10-04 21:42:35 +0900

    create event sample objects

--- app/handler/clean.py
+++ app/handler/clean.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import tornado.web
 
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 
 class CleanHandler(tornado.web.RequestHandler):
@@ -21,19 +22,22 @@ class CleanHandler(tornado.web.RequestHandler):
 		person1 = db.persons.insert_one({'name': u'中原和夫', 'icon': 'person1.png'}).inserted_id.__str__()
 		person2 = db.persons.insert_one({'name': u'足立悦子', 'icon': 'person2.png'}).inserted_id.__str__()
 
-		db.messages.insert_one({'body': u'おはよう',         'created': now, 'groupId': group1, 'personId': person1})
-		db.messages.insert_one({'body': u'こんにちは',       'created': now, 'groupId': group1, 'personId': person2})
-		db.messages.insert_one({'body': u'こんばんは',       'created': now, 'groupId': group1, 'personId': person1})
-		db.messages.insert_one({'body': u'また別のおはよう', 'created': now, 'groupId': group2, 'personId': person2})
+		message1 = db.messages.insert_one({'body': u'おはよう',         'created': now, 'groupId': group1, 'personId': person1}).inserted_id.__str__()
+		message2 = db.messages.insert_one({'body': u'こんにちは',       'created': now, 'groupId': group1, 'personId': person2}).inserted_id.__str__()
+		message3 = db.messages.insert_one({'body': u'こんばんは',       'created': now, 'groupId': group1, 'personId': person1}).inserted_id.__str__()
+		message4 = db.messages.insert_one({'body': u'また別のおはよう', 'created': now, 'groupId': group2, 'personId': person2}).inserted_id.__str__()
 
 		schedule1 = db.schedules.insert_one({'day': u'2015-09-28', 'place': u'高橋区民センター', 'note': u'指揮者不在のため、合奏はありません。', 'created': now, 'groupId': group1, 'personId': person1}).inserted_id.__str__()
 		schedule2 = db.schedules.insert_one({'day': u'2015-10-05', 'place': u'山田市営ホール',   'note': u'', 'created': now, 'groupId': group1, 'personId': person1}).inserted_id.__str__()
 
-		db.attendances.insert_one({'choice': u'1', 'note': u'',                       'created': now, 'scheduleId': schedule1, 'groupId': group1, 'personId': person1})
-		db.attendances.insert_one({'choice': u'2', 'note': u'15時ごろから参加します', 'created': now, 'scheduleId': schedule1, 'groupId': group1, 'personId': person2})
+		attendance1 = db.attendances.insert_one({'choice': u'1', 'note': u'',                       'created': now, 'scheduleId': schedule1, 'groupId': group1, 'personId': person1}).inserted_id.__str__()
+		attendance2 = db.attendances.insert_one({'choice': u'2', 'note': u'15時ごろから参加します', 'created': now, 'scheduleId': schedule1, 'groupId': group1, 'personId': person2}).inserted_id.__str__()
 
-		db.events.insert_one({'body': u'新規イベント1', 'created': now, 'groupId': group1})
-		db.events.insert_one({'body': u'新規イベント2', 'created': now, 'groupId': group1})
-		db.events.insert_one({'body': u'新規イベント3', 'created': now, 'groupId': group2})
+
+		person = db.persons.find_one(ObjectId(person1))
+
+		db.events.insert_one({'type': 'message',    'messageId':  message1,  'body': u'おはよう',   'created': now, 'groupId': group1, 'person': person})
+		db.events.insert_one({'type': 'schedule',   'scheduleId': schedule1, 'day':  u'2015-09-28', 'created': now, 'groupId': group1, 'person': person})
+		db.events.insert_one({'type': 'attendance', 'scheduleId': schedule1, 'attendanceId': attendance1, 'day':  u'2015-09-28', 'choice': '1', 'created': now, 'groupId': group1, 'person': person})
 
 		self.redirect('/login')
